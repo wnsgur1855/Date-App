06a2fabcfc6c3655b330df3dcc87be0d5ff10e99
junhyuck park <wnsgur1855@naver.com>
2023-03-13 19:43:46 +0900

    comment: 설명주석

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -5,6 +5,9 @@ import { getItem } from '../api/axios';
 
 function Detail() {
   const { id } = useParams();
+  //useQuery 훅은 첫 번째 인자로 쿼리 식별자를 받는다. 이 쿼리 식별자는 캐시를 구분하기 위한 역할을 한다.(쿼리 키)
+  //id를 쓴 것은 id값이 변경될 때마다 getItem함수를 호출하여 해당 `id`에 해당하는 데이터를 가져오게 된다.
+
   const { isLoading, isError, data } = useQuery(['posts', id], () => getItem(id));
   if (isLoading) {
     return <div>로딩중</div>;
