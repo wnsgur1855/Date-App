eb6108f66c80104c23585cdae3982a77fd8cc0a6
junhyuck park <wnsgur1855@naver.com>
2023-03-22 04:02:04 +0900

    feat: 모달 애니매이션 적용, alert창 이쁘게 꾸미기

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,9 +9,41 @@ export const ModalContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 450px;
-  height: 500px;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
   border: 1px solid;
+  background-color: red;
+  transition: background-color 0.5s ease;
+  top: ${(props) => props.top};
+  border: 1px solid;
+  & :hover {
+    background-color: blue;
+  }
+`;
+
+export const ModalStyle1 = styled.div`
+  padding: 20px;
+  border-bottom: 1px solid #ddd;
+  font: 300 24px Lato;
+  position: relative;
+`;
+export const ModalStyle2 = styled.div`
+  padding: 20px;
+  font: 300 16px Lato;
+`;
+export const ModalButton = styled.button`
+  background: linear-gradient(to right, rgb(220, 227, 91), rgb(69, 182, 73));
+  padding: 10px 25px;
+  display: inline-block;
+  border-radius: 25px;
+  margin: 20px 0;
+  color: #fff;
+  position: relative;
+  transition: all 0.4s ease;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0 3px 15px rgba(0, 0, 0, 0.2);
+  }
 `;
 
 function Modal() {
@@ -19,6 +51,11 @@ function Modal() {
     nickname: '',
     password: '',
   });
-  return <ModalContainer>{localStorage.getItem(join.nickname) && <Signup />}</ModalContainer>;
+  return !localStorage.getItem('join.nickname') ? (
+    <ModalContainer width="450px" height="500px">
+      <Signup />
+    </ModalContainer>
+  ) : null;
 }
+
 export default Modal;
--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,8 +1,141 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { HomeContainer } from './Home';
+import { SS } from './Home';
+import styled, { keyframes } from 'styled-components';
+import Swal from 'sweetalert2';
 import AudioPlayer from '../components/AudioPlayer';
 
+// 모달 표시 및 숨김을 제어하는 함수
+function useModal(initialState = false) {
+  const [visible, setVisible] = useState(initialState);
+
+  const openModal = () => {
+    setVisible(true);
+  };
+
+  const closeModal = () => {
+    setVisible(false);
+  };
+
+  return { visible, openModal, closeModal };
+}
+
+// 모달 컴포넌트 스타일드 컴포넌트로 정의
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: ${({ visible }) => (visible ? 'flex' : 'none')};
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+`;
+
+const ModalContents = styled.div`
+  background-color: #fff;
+  border-radius: 4px;
+  padding: 20px;
+  animation: ${({ visible }) => (visible ? fadeIn : fadeOut)} 2s;
+`;
+
+// 애니메이션 키프레임 정의
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-500px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+    transform: translateY(1);
+  }
+
+  to {
+    opacity: 0;
+    transform: translateY(-500px);
+  }
+`;
+
+const DDiv = styled.div`
+  animation-duration: 3s;
+  animation: ${({ slide }) => slide && slidein};
+`;
+const slidein = keyframes`
+from {
+    margin-left: 100%;
+    width: 300%
+  }
+
+  to {
+    margin-left: 0%;
+    width: 100%;
+  }
+`;
+
 function Mypage() {
-  return <button>로그아웃</button>;
+  const [join, setJoin] = useState({
+    nickname: '',
+    password: '',
+  });
+
+  console.log(join.nickname);
+  const [slide, setSlide] = useState(true);
+  useEffect(() => {
+    setSlide(true);
+  }, []);
+
+  const { visible, openModal, closeModal } = useModal(false);
+  const navigate = useNavigate();
+  console.log(join.nickname);
+  const logoutHandler = () => {
+    localStorage.removeItem('join.nickname');
+    localStorage.removeItem('join.password');
+    Swal.fire({
+      title: `${join.nickname}님 잘가요😭`,
+      icon: 'warning',
+      confirmButtonText: '확인',
+    }).then(() => navigate('/')); // 확인 버튼을 누르면 '/' 경로로 이동합니다.
+  };
+
+  return (
+    <HomeContainer>
+      <DDiv slide={slide}>안녕하세요 나는 박준혁입니다 지금부터 마술쇼 시작합니다</DDiv>
+      <div>
+        <button onClick={openModal}>모달 열기</button>
+        <ModalWrapper visible={visible}>
+          <ModalContents visible={visible}>
+            <h2>모달 제목</h2>
+            <p>모달 내용</p>
+            <button onClick={closeModal}>닫기</button>
+          </ModalContents>
+        </ModalWrapper>
+      </div>
+      <SS onClick={logoutHandler}>로그아웃</SS>
+    </HomeContainer>
+  );
 }
 
 export default Mypage;
+
+//헤더에있으면 뭐가 됐든 작동이 안 된다?
+
+//로컬스토리지 key값 잘 확인하자
+
+// slidein은 keyframes를 선언한 변수이기 때문에 DDiv에서 animation에 slidein을 전달할 때 props로 전달하는 것이 아니라,
+//     keyframes의 이름 그대로 전달해야 합니다.
+//     또한, DDiv의 slide 속성에 true를 전달하는 것이 아니라,
+//         slide 상태값을 전달해주어야 합니다.
+
+//이쁜 alert창 만들기
